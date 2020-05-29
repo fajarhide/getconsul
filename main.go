2c97d841684bb37cafdebcb98efcd4c9ba1d0821
fajarhide <fajarhide@gmail.com>
2020-05-29 15:00:23 +0700

    no update env if endpoint down

--- main.go
+++ main.go
@@ -18,55 +18,61 @@ type Data struct {
 }
 
 func main() {
-	for {
-		err := config.Load(".env")
+	err := config.Load(".env")
   		if err != nil {
 			log.Fatal("Error loading .env file")
 			os.Exit(2)
-  		}
-		
-		f, err := os.Create(fmt.Sprintf("%s", os.Getenv("KEY")))
-    	if err != nil {
-    	    fmt.Println(err)
-    	    return
 		}
-		
-		t := time.Now()
-	
-		resp, err := http.Get(fmt.Sprintf("https://%s@%s/v1/kv/%s/%s/env?token=%s",os.Getenv("BASIC_AUTH"), os.	Getenv("URL_CONSUL"), os.Getenv("APP"), os.Getenv("ENV"), os.Getenv("TOKEN")))
-		if err != nil {
-			log.Fatalln(err)
+
+	for {
+		get()
 		}
-	
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	
-		var data []Data
-		json.Unmarshal([]byte(body), &data)
-		uDec, err := base64.StdEncoding.DecodeString(string(data[0].Value))
-		if err != nil {
-			fmt.Printf("Error decoding string: %s ", err.Error())
-			return
-		}
-	
-		l, err := f.WriteString(string(uDec))
-    	if err != nil {
-    	    fmt.Println(err)
-    	    f.Close()
-    	    return
-    	}
-    	fmt.Println(l, "bytes written successfully")
-    	err = f.Close()
-    	if err != nil {
-    	    fmt.Println(err)
-    	    return
-		}
-		
-		i := os.Getenv("INTERVAL")
-		interval, err := strconv.Atoi(i)
-		fmt.Println("Updated at", t.Format("15:04:05 2006-01-02 \n"))
-		time.Sleep( time.Duration(interval)  * time.Second)
+}
+
+func get() {
+	resp, err := http.Get(fmt.Sprintf("https://%s@%s/v1/kv/%s/%s/env?token=%s",os.Getenv("BASIC_AUTH"), os.	Getenv("URL_CONSUL"), os.Getenv("APP"), os.Getenv("ENV"), os.Getenv("TOKEN")))
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-}	
+	
+	f, err := os.Create(fmt.Sprintf("%s", os.Getenv("KEY")))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	
+	t := time.Now()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var data []Data
+	json.Unmarshal([]byte(body), &data)
+	uDec, err := base64.StdEncoding.DecodeString(string(data[0].Value))
+	if err != nil {
+		fmt.Printf("Error decoding string: %s ", err.Error())
+		return
+	}
+
+	l, err := f.WriteString(string(uDec))
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	fmt.Println(l, "bytes written successfully")
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Updated at", t.Format("15:04:05 2006-01-02 \n"))
+	i := os.Getenv("INTERVAL")
+	interval, err := strconv.Atoi(i)
+	time.Sleep( time.Duration(interval) * time.Second)
+}
